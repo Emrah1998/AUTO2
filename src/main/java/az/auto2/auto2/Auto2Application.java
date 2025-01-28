490d805f648f2a92fc08a1fe751be22bfea5ac2d
Emrah1998 <emrahqarawov240@gmail.com>
2025-01-28 11:17:48 +0400

    first commit

---
+++ src/main/java/az/auto2/auto2/Auto2Application.java
@@ -0,0 +1,13 @@
+package az.auto2.auto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auto2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auto2Application.class, args);
+    }
+
+}
---
+++ src/test/java/az/auto2/auto2/Auto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.auto2.auto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auto2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
